4d515229eac98e1dbcbbe44a76e8a0f981961c7f
Ferreira <Natanael.Ferreira@Dellteam.com>
2021-11-20 00:13:15 -0300

    Estudos C#

---
+++ Treinamento.Versatile.UIAutomatedTesting/Teste.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treinamento.Versatile.UIAutomatedTesting
+{
+    public class Teste
+    {
+        [Test]
+       public void Test1() 
+        {
+            string nome = "Natan";
+            int idade = 29;
+            Boolean genero = true;
+
+            Console.WriteLine($"Ola meu nome é {nome} tenho {idade} de idade e sou homem {genero}" );
+        }
+
+    }
+}
